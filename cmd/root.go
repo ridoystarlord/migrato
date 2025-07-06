e82efbaa1f4a1cfef634d92ddcec374d0c3cd56f
Ridoy <ridoy.starlord@gmail.com>
2025-07-06 11:23:03 +0600

    Bump version to v1.0.5

--- cmd/root.go
+++ cmd/root.go
@@ -7,7 +7,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var Version = "v1.0.0"
+var Version = "v1.0.5"
 
 var rootCmd = &cobra.Command{
 	Use:   "migrato",
--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import "github.com/ridoystarlord/migrato/cmd"
 
-var version = "v1.0.0"
+var version = "v1.0.5"
 
 func main() {
 	cmd.Version = version
